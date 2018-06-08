e6a24241cf754378846617e0cf255769b734919d
KamilPlisWSIZ <w57449@student.wsiz.rzeszow.pl>
2018-06-08 18:33:27 +0200

    Dodanie menu bocznego.

--- app/src/main/java/com/example/mediaapp/MainActivity.java
+++ app/src/main/java/com/example/mediaapp/MainActivity.java
@@ -1,52 +1,33 @@
 package com.example.mediaapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+
+import android.view.MenuItem;
+
+
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button button1;
-    private Button button2;
-
-
-
+    private DrawerLayout mDrawerLayout ;
+    private ActionBarDrawerToggle mToggle ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openVideosList();
-            }
-        });
-
-        button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openVideoPlayer();
-            }
-        });
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
-
-
-    //button methods
-    public void openVideoPlayer(){
-        Intent intent = new Intent(this, VideoPlayer.class );
-        startActivity(intent);
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
-
-    public void openVideosList(){
-        Intent intent = new Intent(this, VideosList.class );
-        startActivity(intent);
-    }
-
-
 }
