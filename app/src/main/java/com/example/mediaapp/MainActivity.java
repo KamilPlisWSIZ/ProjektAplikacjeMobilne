ac8a5f713cafeed2616bf114e1c390910e9c07eb
KamilPlisWSIZ <w57449@student.wsiz.rzeszow.pl>
2018-06-09 08:40:17 +0200

    Menu boczne, dodanie fragmentow.

--- app/src/main/java/com/example/mediaapp/MainActivity.java
+++ app/src/main/java/com/example/mediaapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.mediaapp;
 
+import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,23 +16,63 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
-    private DrawerLayout mDrawerLayout ;
-    private ActionBarDrawerToggle mToggle ;
+    private DrawerLayout dl ;
+    private ActionBarDrawerToggle toggle ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open,R.string.close);
-        mDrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();
+        dl = (DrawerLayout) findViewById(R.id.dl);
+        toggle = new ActionBarDrawerToggle(this,dl,R.string.open,R.string.close);
+        dl.addDrawerListener(toggle);
+        NavigationView nvDrawer = (NavigationView) findViewById(R.id.nv);
+        toggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setupDrawerContent(nvDrawer);
+    }
+
+    public void selectItemDrawer(MenuItem menuItem){
+        Fragment myFragment = null;
+        Class fragmentClass;
+        switch (menuItem.getItemId()){
+            case R.id.videoslist:
+                fragmentClass = VidsListFragment.class;
+                break;
+
+            default:
+                fragmentClass = FavoritesFragment.class;
+        }
+try{
+            myFragment = (Fragment) fragmentClass.newInstance();
+}
+catch(Exception e){
+            e.printStackTrace();
+        }
+FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flcontent, myFragment).commit();
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        dl.closeDrawers();
 
     }
+
+    private void setupDrawerContent(NavigationView navigationView){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectItemDrawer(item);
+                return true;
+            }
+        });
+    }
+
+
     public boolean onOptionsItemSelected(MenuItem item){
-        if(mToggle.onOptionsItemSelected(item)){
+        if(toggle.onOptionsItemSelected(item)){
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
