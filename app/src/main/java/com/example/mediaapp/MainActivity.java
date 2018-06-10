ee891c33e20fe3c209bc06b28cbcd154ff00ab88
KamilPlisWSIZ <w57449@student.wsiz.rzeszow.pl>
2018-06-11 00:54:32 +0200

    Dodanie możliwości dodawania do ulubionych. Dodany realm.

--- app/src/main/java/com/example/mediaapp/MainActivity.java
+++ app/src/main/java/com/example/mediaapp/MainActivity.java
@@ -5,13 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
-import android.widget.Button;
 
+import com.example.mediaapp.favorites.FavoritesActivity;
 import com.example.mediaapp.topsongs.TopSongsActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-private CardView videosIcon, vidslistIcon, favsIcon, musicplayerIcon;
+private CardView videosIcon, vidslistIcon, favoritesIcon, musicplayerIcon, topalbumsIcon;
 
 
 
@@ -22,15 +22,16 @@ private CardView videosIcon, vidslistIcon, favsIcon, musicplayerIcon;
 
         videosIcon = (CardView) findViewById(R.id.videosicon);
         vidslistIcon = (CardView) findViewById(R.id.vidslisticon);
-        favsIcon = (CardView) findViewById(R.id.favsicon);
+        topalbumsIcon = (CardView) findViewById(R.id.topalbumsicon);
+        favoritesIcon = (CardView) findViewById(R.id.favoritesicon);
         musicplayerIcon = (CardView) findViewById(R.id.musicplayericon);
 
 
         videosIcon.setOnClickListener(this);
         vidslistIcon.setOnClickListener(this);
-        favsIcon.setOnClickListener(this);
+        topalbumsIcon.setOnClickListener(this);
         musicplayerIcon.setOnClickListener(this);
-
+        favoritesIcon.setOnClickListener(this);
     }
 
 
@@ -52,8 +53,9 @@ Intent i;
         switch (v.getId()){
             case R.id.videosicon : i = new Intent(this,VideoPlayer.class);startActivity(i); break;
             case R.id.vidslisticon : i = new Intent(this,VideosList.class);startActivity(i); break;
-            case R.id.favsicon : i = new Intent(this,TopSongsActivity.class);startActivity(i); break;
+            case R.id.topalbumsicon : i = new Intent(this,TopSongsActivity.class);startActivity(i); break;
             case R.id.musicplayericon : i = new Intent(this,MusicPlayer.class);startActivity(i); break;
+            case R.id.favoritesicon : i = new Intent(this,FavoritesActivity.class);startActivity(i); break;
             default:break;
         }
     }
