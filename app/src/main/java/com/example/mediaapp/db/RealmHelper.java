cbd77ed33bb565e1a58a70221c83861a8d210053
KamilPlisWSIZ <w57449@student.wsiz.rzeszow.pl>
2018-06-12 21:09:18 +0200

    Modyfikacja funkcji wyświetlania ulubionych.

---
+++ app/src/main/java/com/example/mediaapp/db/RealmHelper.java
@@ -0,0 +1,32 @@
+package com.example.mediaapp.db;
+
+import java.util.ArrayList;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+import io.realm.Sort;
+
+public class RealmHelper {
+
+    Realm realm;
+    public RealmHelper(Realm realm){
+        this.realm = realm;
+    }
+
+
+    public ArrayList<String> retrieve(){
+        ArrayList<String> favoriteAlbums=new ArrayList<>();
+        RealmResults<Favorite> favorites = realm
+                .where(Favorite.class )
+                .sort( "date", Sort.DESCENDING )
+                .findAll();
+    for(Favorite s:favorites){
+        favoriteAlbums.add(s.getAlbum());
+
+    }
+return favoriteAlbums;
+    }
+
+
+
+}
--- app/src/main/java/com/example/mediaapp/favorites/FavoritesActivity.java
+++ app/src/main/java/com/example/mediaapp/favorites/FavoritesActivity.java
@@ -1,28 +1,67 @@
 package com.example.mediaapp.favorites;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import com.example.mediaapp.MainActivity;
 import com.example.mediaapp.R;
 import com.example.mediaapp.db.Favorite;
+import com.example.mediaapp.db.RealmHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import io.realm.Realm;
+import io.realm.RealmConfiguration;
 import io.realm.RealmResults;
 import io.realm.Sort;
 public class FavoritesActivity extends AppCompatActivity {
+
+android.support.v7.widget.RecyclerView rvList;
+Realm realm;
+ArrayList<String> favorites;
+//ArrayList <String> favorites = new ArrayList<String>();
+    ArrayAdapter adapter;
+ListView lv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); setContentView(R.layout.activity_favorites);
-        Realm realm = Realm.getDefaultInstance ();
-        RealmResults<Favorite> favorites = realm
-                .where(Favorite.class )
-                .sort( "date", Sort.DESCENDING )
-                .findAll();
-        if (favorites.size() > 0) {
-            Toast.makeText ( this , "Pobrano ulubione" ,Toast.LENGTH_SHORT).show();
-            for (Favorite favorite : favorites) { Log.d ( "FAV" , favorite.getAlbum() + " - " + favorite.getArtist());
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites);
+
+
+        lv = (ListView) findViewById(R.id.lv);
+        Realm.init(getApplicationContext());
+        RealmConfiguration config = new RealmConfiguration.
+                Builder().
+                deleteRealmIfMigrationNeeded().
+                build();
+        Realm.setDefaultConfiguration(config);
+        realm = Realm.getInstance(config);
+        RealmHelper helper = new RealmHelper(realm);
+        favorites=helper.retrieve();
+
+        //Bindowanie
+        adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,favorites);
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(FavoritesActivity.this,favorites.get(position),Toast.LENGTH_SHORT);
             }
-        }
-        else { Toast.makeText ( this , "Brak ulubionych" ,Toast. LENGTH_SHORT).show();
-        }
+        });
+
+
+
+
+
+
     }
 }
